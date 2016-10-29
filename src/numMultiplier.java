0920b7edcee760dac5c0cca7ae76d542fd2e3de9
colerud02 <league@WTS7.attlocal.net>
2016-10-28 18:32:53 -0700

    h

--- src/numMultiplier.java
+++ src/numMultiplier.java
@@ -1,10 +1,10 @@
-
+//need to not multiply by current index that im on
 public class numMultiplier { public static void main (String [] args){
 	int total = 1;
 	int [] nums = {2, 3 ,1,4,3,2};
 	int x = nums.length;
 	for (int i = 0; i < x; i++) {
-		int secNum = nums[i];
+		nums[i] = 1;
 		for (int j = 0; j < (x-1); j++) {
 			
 			total = total * nums[j];
