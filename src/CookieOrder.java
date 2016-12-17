d7d6a722a8a5553f73f996105c0558af5297a68c
Leauge Mac <DONT CHANGE THIS!!!!!>
2016-12-16 18:38:46 -0800

    nyehehe

---
+++ src/CookieOrder.java
@@ -0,0 +1,21 @@
+
+public class CookieOrder {
+	String variety;
+	int numBoxes;
+	public CookieOrder(String variety, int numBoxes) {
+		// TODO Auto-generated constructor stub
+	}
+	public String getVariety() {
+		return variety;
+	}
+	public void setVariety(String variety) {
+		this.variety = variety;
+	}
+	public int getNumBoxes() {
+		return numBoxes;
+	}
+	public void setNumBoxes(int numBoxes) {
+		this.numBoxes = numBoxes;
+	}
+
+}
